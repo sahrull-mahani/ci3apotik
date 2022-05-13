b9cf76d6a2ed3c65429313acd9c1119474ea0578
sahrul mahani <sahrul.mahani@gmail.com>
2022-05-13 13:48:18 +0800

    mengubah

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -128,7 +128,7 @@
       <!-- Brand Logo -->
       <a href="<?= base_url(); ?>" class="brand-link">
         <img src="<?= base_url('assets'); ?>/img/makna2.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">PT. Makna</span>
+        <span class="brand-text font-weight-light">PT. Makna Diubah</span>
       </a>
 
       <!-- Sidebar -->
